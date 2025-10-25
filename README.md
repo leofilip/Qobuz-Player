94dc1e40b4b6f970f6e89bb1c9ae494a76daa079
Leonardo Filipe <222072979+leofilip@users.noreply.github.com>
2025-10-25 22:45:10 +0100

    feat: update project configuration and enhance tray functionality

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+## Important Note: Do Not Use WSL
+
+Do not use Windows Subsystem for Linux (WSL) to build or run this project. Use the native Windows environment for all development and execution to avoid compatibility issues and runtime errors.
 
 # Qobuz Player
 
@@ -19,3 +22,34 @@ This template should help get you started developing with Tauri in vanilla HTML,
 ## Recommended IDE Setup
 
 - [VS Code](https://code.visualstudio.com/) + [Tauri](https://marketplace.visualstudio.com/items?itemName=tauri-apps.tauri-vscode) + [rust-analyzer](https://marketplace.visualstudio.com/items?itemName=rust-lang.rust-analyzer)
+
+
+## Project Dependencies
+
+- [Rust](https://www.rust-lang.org/tools/install)
+- [Cargo](https://doc.rust-lang.org/cargo/getting-started.html)
+- [Tauri CLI](https://tauri.app/)
+- Node.js (for Tauri frontend, if applicable)
+
+## Windows Build Troubleshooting
+
+If you encounter the error:
+
+```
+error: incremental compilation: could not create session directory lock file: Incorrect function. (os error -2147024895)
+```
+
+You can fix it by disabling incremental compilation or changing the build directory:
+
+**Disable incremental compilation:**
+```cmd
+set CARGO_INCREMENTAL=0
+cargo build
+```
+
+**Change build directory:**
+```cmd
+set CARGO_TARGET_DIR=C:\temp\target
+cargo build
+```
+
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,66 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::{
+    menu::{Menu, MenuItem},
+    tray::{TrayIconBuilder, MouseButtonState, MouseButton, TrayIconEvent},
+    Manager, WindowEvent,
+};
+
 fn main() {
-    qobuz_player_lib::run()
+    tauri::Builder::default()
+        .setup(|app| {
+            // Build menu items
+            let show = MenuItem::with_id(app, "show", "Show", true, None::<&str>)?;
+            let quit = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
+            let menu = Menu::with_items(app, &[&show, &quit])?;
+
+            // Create tray icon
+            TrayIconBuilder::new()
+                .icon(app.default_window_icon().unwrap().clone())
+                .menu(&menu)
+                .show_menu_on_left_click(false)
+                .on_menu_event(|app, event| match event.id().as_ref() {
+                    "quit" => {
+                        println!("Quitting application...");
+                        app.exit(0);
+                    }
+                    "show" => {
+                        if let Some(window) = app.get_webview_window("main") {
+                            window.show().unwrap();
+                            window.set_focus().unwrap();
+                        }
+                    }
+                    _ => {
+                        println!("Unknown menu item {:?} clicked", event.id());
+                    }
+                })
+                .on_tray_icon_event(|tray, event| match event {
+                    TrayIconEvent::Click{
+                        button: MouseButton::Left,
+                        button_state: MouseButtonState::Up,
+                        ..
+                    } => {
+                        let app = tray.app_handle();
+                        if let Some(window) = app.get_webview_window("main") {
+                            let _ = window.unminimize();
+                            let _ = window.show();
+                            let _ = window.set_focus();
+                        }
+                    }
+                    _ => { }
+                })
+                .build(app)?;
+            Ok(())
+        })
+        .on_window_event(|app, event| {
+            // match the close request on the window event and hide instead of closing
+            if let WindowEvent::CloseRequested { api, .. } = event {
+                api.prevent_close();
+                if let Some(window) = app.get_webview_window("main") {
+                    window.hide().unwrap();
+                }
+            }
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
